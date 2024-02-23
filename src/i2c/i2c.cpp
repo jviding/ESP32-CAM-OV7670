357625144f986571a73d666e7a1e2aef5318aaf1
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-02-23 02:39:31 +0200

    i2c

---
+++ src/i2c/i2c.cpp
@@ -0,0 +1,102 @@
+#include "i2c.h"
+
+
+i2c_port_t I2C::port;
+i2c_cmd_handle_t I2C::cmd;
+SemaphoreHandle_t I2C::i2c_handle;
+
+
+esp_err_t I2C::init(i2c_conf_t i2c_conf) {
+  ESP_LOGD(TAG, "Initializing I2C...");
+  ESP_LOGD(TAG, " - sda: gpio %d", i2c_conf.pin_sda);
+  ESP_LOGD(TAG, " - scl: gpio %d", i2c_conf.pin_scl);
+  ESP_LOGD(TAG, " - freq: %ld",    i2c_conf.freq);
+  ESP_LOGD(TAG, " - port: %d",     i2c_conf.port);
+  // Config
+  const i2c_config_t conf = {
+    .mode =           I2C_MODE_MASTER,
+    .sda_io_num =     i2c_conf.pin_sda,
+    .scl_io_num =     i2c_conf.pin_scl,
+    .sda_pullup_en =  GPIO_PULLUP_ENABLE,
+    .scl_pullup_en =  GPIO_PULLUP_ENABLE,
+    .master = { 
+      .clk_speed =    i2c_conf.freq,
+    },
+    .clk_flags =      0,
+  };
+  // Configure and enable
+  ESP_RETURN_ON_ERROR(i2c_param_config(i2c_conf.port, &conf), TAG, "Failed configuring I2C bus.");
+  ESP_RETURN_ON_ERROR(i2c_driver_install(i2c_conf.port, I2C_MODE_MASTER, 0, 0, 0), TAG, "Failed installing I2C driver.");
+  // Create and enable Semaphore
+  i2c_handle = xSemaphoreCreateBinary();
+  ESP_RETURN_ON_FALSE(i2c_handle != NULL, ESP_ERR_NO_MEM, TAG, "Failed creating I2C semaphore.");
+  ESP_RETURN_ON_FALSE(xSemaphoreGive(i2c_handle) == pdTRUE, ESP_ERR_NO_MEM, TAG, "Failed giving I2C semaphore upon initialization.");
+  // Set port
+  port = i2c_conf.port;
+  // Done
+  ESP_LOGD(TAG, "Successfully initialized I2C.");
+  return ESP_OK;
+};
+
+esp_err_t I2C::start() {
+  cmd = i2c_cmd_link_create();
+  ESP_RETURN_ON_FALSE(cmd != NULL, ESP_ERR_NO_MEM, TAG, "Failed creating I2C command list.");
+  ESP_RETURN_ON_ERROR(i2c_master_start(cmd), TAG, "Failed pushing 'Start signal' to I2C command list.");
+  // Done
+  ESP_LOGD(TAG, "Successfully started I2C command list.");
+  return ESP_OK;
+};
+
+esp_err_t I2C::stop() {
+  ESP_RETURN_ON_ERROR(i2c_master_stop(cmd), TAG, "Failed pushing 'Stop signal' to I2C command list.");
+  ESP_RETURN_ON_ERROR(i2c_master_cmd_begin(port, cmd, portMAX_DELAY), TAG, "Failed transmitting I2C command list.");
+  i2c_cmd_link_delete(cmd);
+  // Done
+  ESP_LOGD(TAG, "Successfully stopped and transmitted I2C command list.");
+  return ESP_OK;
+};
+
+esp_err_t I2C::write(uint8_t val) {
+  ESP_RETURN_ON_ERROR(i2c_master_write_byte(cmd, val, false), TAG, "Failed pushing 'write byte' to I2C command list");
+  // Done
+  ESP_LOGD(TAG, "Successfully pushed 'write byte' to I2C command list.");
+  return ESP_OK;
+};
+
+esp_err_t I2C::read(uint8_t* val) {
+  ESP_RETURN_ON_ERROR(i2c_master_read_byte(cmd, val, I2C_MASTER_NACK), TAG, "Failed pushing 'read byte' to I2C command list.");
+  // Done
+  ESP_LOGD(TAG, "Successfully pushed 'read byte' to I2C command list.");
+  return ESP_OK;
+};
+
+esp_err_t I2C::write_register(uint8_t reg, uint8_t val) {
+  ESP_LOGD(TAG, "Called write_register(reg: %02x, val: %02x)...", reg, val);
+  ESP_RETURN_ON_FALSE(xSemaphoreTake(i2c_handle, portMAX_DELAY) == pdTRUE, ESP_ERR_TIMEOUT, TAG, "Failed taking I2C semaphore for writing.");
+  ESP_RETURN_ON_ERROR(start(),           TAG, "Failed I2C start in write_register.");
+  ESP_RETURN_ON_ERROR(write(WRITE_ADDR), TAG, "Failed I2C write (write address) in write_register.");
+  ESP_RETURN_ON_ERROR(write(reg),        TAG, "Failed I2C write (register address) in write_register.");
+  ESP_RETURN_ON_ERROR(write(val),        TAG, "Failed I2C write (register value) in write_register.");
+  ESP_RETURN_ON_ERROR(stop(),            TAG, "Failed I2C stop in write_register.");
+  ESP_RETURN_ON_FALSE(xSemaphoreGive(i2c_handle) == pdTRUE, ESP_ERR_NO_MEM, TAG, "Failed giving I2C semaphore after writing.");
+  // Done
+  ESP_LOGD(TAG, "Successfully wrote the value to register.");
+  return ESP_OK;
+};
+
+esp_err_t I2C::read_register(uint8_t reg, uint8_t* val) {
+  ESP_LOGD(TAG, "Called read_register(reg: %02X, *val)...", reg);
+  ESP_RETURN_ON_FALSE(xSemaphoreTake(i2c_handle, portMAX_DELAY) == pdTRUE, ESP_ERR_TIMEOUT, TAG, "Failed taking I2C semaphore for reading.");
+  ESP_RETURN_ON_ERROR(start(),           TAG, "Failed I2C first start in read_register.");
+  ESP_RETURN_ON_ERROR(write(WRITE_ADDR), TAG, "Failed I2C write (write address) in read_register.");
+  ESP_RETURN_ON_ERROR(write(reg),        TAG, "Failed I2C write (register address) in read_register.");
+  ESP_RETURN_ON_ERROR(stop(),            TAG, "Failed I2C first stop in read_register.");
+  ESP_RETURN_ON_ERROR(start(),           TAG, "Failed I2C second start in read_register.");
+  ESP_RETURN_ON_ERROR(write(READ_ADDR),  TAG, "Failed I2C write (read address) in read_register.");
+  ESP_RETURN_ON_ERROR(read(val),         TAG, "Failed I2C read (register value) in read_register.");
+  ESP_RETURN_ON_ERROR(stop(),            TAG, "Failed I2C second stop in read_register.");
+  ESP_RETURN_ON_FALSE(xSemaphoreGive(i2c_handle) == pdTRUE, ESP_ERR_NO_MEM, TAG, "Failed giving I2C semaphore after reading.");
+  // Done
+  ESP_LOGD(TAG, "Successfully read the value from register.");
+  return ESP_OK;
+};
---
+++ src/i2c/i2c.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "esp_check.h"
+#include "esp_log.h"
+#include <stdint.h>
+
+#define TAG           "I2C"
+
+#define DEVICE_ADDR   0x21
+#define WRITE_ADDR    ((DEVICE_ADDR << 1) | 0x00)
+#define READ_ADDR     ((DEVICE_ADDR << 1) | 0x01)
+
+
+struct i2c_conf_t {
+  gpio_num_t pin_sda = GPIO_NUM_26; // Default to 26
+  gpio_num_t pin_scl = GPIO_NUM_27; // Default to 27
+  uint32_t   freq    = 200 * 1000;  // Default to 200 kHz
+  i2c_port_t port    = I2C_NUM_0;   // Default to port0
+};
+
+class I2C {
+  public:
+    // Functions
+    static esp_err_t init(i2c_conf_t i2c_conf);
+    static esp_err_t write_register(uint8_t reg, uint8_t val);
+    static esp_err_t read_register(uint8_t reg, uint8_t* val);
+
+  private:
+    // Variables
+    static i2c_port_t port;
+    static i2c_cmd_handle_t cmd;
+    static SemaphoreHandle_t i2c_handle;
+
+    // Functions
+    static esp_err_t start();
+    static esp_err_t stop();
+    static esp_err_t write(uint8_t val);
+    static esp_err_t read(uint8_t* val);
+};
